dbb3aec60433de6c32ae8c5e71654ac7cd43dec5
elliot richardson <ear78@aol.com>
2017-02-22 15:55:54 -0600

    switched over to ng-routing and added some other features to fix firefox bugs

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,6 @@
 angular.module('myApp')
 
-    .controller('MainCtrl', function($scope, $state, $window, $location, $anchorScroll, $rootScope){
+    .controller('MainCtrl', function($scope, $route, $state, $window, $location, $anchorScroll, $rootScope){
         // USED FOR CONTACT DIRECTIVE NG-SHOW/HIDE IF ON CONTACT
         $scope.$state = $state;
 
